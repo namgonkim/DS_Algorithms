02c01a12459a7903ca8e5dbcf6bd8990910de5c9
namgonkim <32921225+namgonkim@users.noreply.github.com>
2021-04-29 15:08:44 +0900

    2021-04-29[탐색/이진탐색]
    2021-04-29[탐색/이진탐색]

---
+++ 동빈나/chapter07_탐색_떡볶이떡만들기/main.cpp
@@ -0,0 +1,67 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<utility>
+#include<stdio.h>
+/*
+제목: 떡볶이 떡 만들기
+문제: 손님이 요청한 총길이만큼의 떡을 얻기 위해 절단기에 설정할 수 있는 높이의 최대값을 구하는 프로그램
+유형: 이진 탐색
+날짜: 2021-04-29
+이름: @namgonkim
+*/
+using namespace std;
+
+int n, m; // 떡 개수, 손님이 요청한 떡의 길이
+vector<int> riceCake; // 떡집에 있는 떡 배열
+
+/* 이진 탐색을 이용해 최대한 덜 짜르는 선에서 최대 높이의 떡을 제공하도록 한다 */
+int binary_search(vector<int>& riceCake, int target, int start, int end) {
+    
+    int finder = 0;
+    while (start <= end) {
+        int mid = (start + end) / 2;
+        int lestCakeSum = 0;
+        // 2-1. 자른 떡을 계산한다.
+        for (int i = 0; i < n; i++) {
+            // 2-2. 자르려는 길이보다 큰 떡만 자르고 계산한다.
+            if (mid < riceCake[i])
+                lestCakeSum = lestCakeSum + (riceCake[i] - mid);
+        }
+        // 2-3. 자르고 합한 떡 길이가 손님이 요청한 최대한의 떡길이와 일치한 경우
+        if (lestCakeSum == target) {
+            return mid;
+        }
+        // 2-4. 자르고 합한 떡 길이가 작을 경우(떡이 부족함) 더 많이 자르기
+        else if (lestCakeSum < target) {
+            end = mid - 1;
+        }
+        // 2-5. 자르고 합한 떡 길이가 큰 경우(떡의 양이 충분함) 덜 자르기
+        else {
+            start = mid + 1;
+            // 2-6. 최대한 제공해줄 수 있는 떡의 길이 저장
+            finder = mid;
+        }
+    }
+    return finder;
+}
+
+int solution() {
+    // 1. 젤 긴 떡 길이 찾기
+    int maxCake = *max_element(riceCake.begin(), riceCake.end());
+    // 2. 이진 탐색을 활용해 최대 높이 찾기
+    int answer = binary_search(riceCake, m, 0, maxCake);
+    return answer;
+}
+
+int main() {
+    cin >> n >> m; 
+    riceCake.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> riceCake[i];
+    }
+    int answer = solution();
+    cout << answer << endl;
+    return 0;
+}
---
+++ 동빈나/chapter07_탐색_부품찾기/main.cpp
@@ -0,0 +1,74 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<utility>
+#include<stdio.h>
+/*
+제목: 부품 찾기
+문제: 손님이 요청한 부품이 매장에 있으면 yes, 없으면 no 출력하는 프로그램
+유형: 이진 탐색
+날짜: 2021-04-29
+이름: @namgonkim
+*/
+using namespace std;
+
+int n, m;
+
+/* 이진 탐색 소스코드 */
+bool binary_search(vector<int>data, int target, int start, int end) {
+    while (start <= end) {
+        // 1. 중간 인덱스 설정
+        int mid = (start + end) / 2; 
+        // 2. 타겟을 찾으면 종료
+        if (data[mid] == target) {
+            return true;
+        }
+        // 3. 타겟이 중간 인덱스의 값보다 작다면 끝 인덱스 = 중간 인덱스 -1 로 변경(탐색 경우를 줄여나감)
+        else if (data[mid] > target) {
+            end = mid - 1;
+        }
+        // 4. 타겟이 중간 인덱스 값보다 크다면 첫 인덱스 = 중간 인덱스 + 1로 변경
+        else {
+            start = mid + 1;
+        }
+    }
+    return false;
+}
+
+/* 찾으려는 타겟을 순차적으로 이진 탐색 알고리즘에 적용시켜 찾는다. */
+vector<string> solution(vector<int>&data, vector<int>&finder) {
+    vector<string> answer;
+    for (int i = 0; i < m; i++) {
+        bool exists = binary_search(data, finder[i], 0, n - 1);
+        if (exists == true) {
+            answer.push_back("yes");
+        }
+        else {
+            answer.push_back("no");
+        }
+    }
+    
+    return answer;
+}
+
+int main() {
+
+    cin >> n;
+    vector<int> data(n);
+    for (int i = 0; i < n; i++) {
+        cin >> data[i];
+    }
+    cin >> m;
+    vector<int> finder(m);
+    for (int i = 0; i < m; i++) {
+        cin >> finder[i];
+    }
+    vector<string> answer = solution(data, finder);
+
+    for (int i = 0; i < answer.size(); i++) {
+        cout << answer[i] << " ";
+    }
+
+    return 0;
+}
