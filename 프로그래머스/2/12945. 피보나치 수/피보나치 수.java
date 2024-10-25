ab33cee85a954524586180e6e0274778973b731d
Namgon <zxcv9455@naver.com>
2024-10-25 15:05:20 +0900

    [level 2] Title: 피보나치 수, Time: 6.34 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        int answer = 0;
+        for(int i=2;i<=n;i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567; 
+        }
+        answer = dp[n];
+        return answer;
+    }
+}
