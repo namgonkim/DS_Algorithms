dbbee7b2510b82d4fba4a8732953be64c99be27b
Namgon <zxcv9455@naver.com>
2024-11-03 00:49:47 +0900

    [level 3] Title: 베스트앨범, Time: 1.74 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=java#) 
 
 ### 성능 요약
 
-메모리: 71.9 MB, 시간: 1.81 ms
+메모리: 79.1 MB, 시간: 1.74 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 18일 23:16:00
+2024년 11월 03일 00:49:45
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,57 +1,65 @@
 import java.util.*;
 class Solution {
     static class Music implements Comparable<Music> {
-        int index;
+        int idx;
         int play;
-        int genrePlay;
         
-        public Music(int index, int play) {
-            this.index = index;
+        public Music(int idx, int play) {
+            this.idx = idx;
             this.play = play;
         }
         
         @Override
         public int compareTo(Music o) {
+            // 장르 내에서 가장 많이 재생된 노래 순이지만, 재생된 횟수가 같으면 고유번호가 낮은 노래 먼저 
             if(this.play == o.play) {
-                return Integer.compare(this.index, o.index);
-            } else {
-                return Integer.compare(o.play, this.play);
+                return Integer.compare(this.idx, o.idx);
             }
+            return Integer.compare(o.play, this.play);
         }
     }
-    // 노래 장르 별 재생 횟수 
-    public static Map<String, Integer> gmap = new HashMap<>();
-    public static Map<String, PriorityQueue<Music>> mmap = new HashMap<>();
-    
+    public static Map<String, Integer> gPlayMap = new HashMap<>();
+    public static Map<String, PriorityQueue<Music>> gMusicMap = new HashMap<>();
     public int[] solution(String[] genres, int[] plays) {
-        for(int i=0; i<genres.length; i++) {
-            if(!gmap.containsKey(genres[i])) {
-                gmap.put(genres[i], plays[i]);
-                PriorityQueue<Music> pq = new PriorityQueue<>();
-                pq.offer(new Music(i, plays[i]));
-                mmap.put(genres[i], pq);
+        for(int i=0;i<genres.length;i++) {
+            // 장르별 재생횟수 맵에 삽입 
+            if(!gPlayMap.containsKey(genres[i])) {
+                gPlayMap.put(genres[i], plays[i]);
             } else {
-                gmap.put(genres[i], gmap.get(genres[i])+plays[i]);
-                PriorityQueue<Music> pq = mmap.get(genres[i]);
-                pq.offer(new Music(i, plays[i]));
-                mmap.put(genres[i], pq);
+                gPlayMap.put(genres[i], gPlayMap.get(genres[i]) + plays[i]);
             }
+            // System.out.println("genre: " + genres[i] + ", play: " + gPlayMap.get(genres[i]));
+            // 장르별 노래 삽입 
+            Music music = new Music(i, plays[i]);
+            PriorityQueue<Music> pq;
+            if(!gMusicMap.containsKey(genres[i])) {
+                pq = new PriorityQueue<>();
+            } else {
+                pq = gMusicMap.get(genres[i]);
+            }
+            pq.offer(music);
+            gMusicMap.put(genres[i], pq);
         }
+        // System.out.println(gPlayMap.size());
         
-        List<String> genreSet = new ArrayList<>(gmap.keySet());
-        genreSet.sort((o1, o2) -> gmap.get(o2).compareTo(gmap.get(o1)));
+        // 장르별 재생횟수 키셋 정렬 
+        List<String> keyList = new ArrayList<>(gPlayMap.keySet());
+        keyList.sort((o1, o2) -> gPlayMap.get(o1).compareTo(gPlayMap.get(o2)));
         
         List<Integer> result = new ArrayList<>();
-        for(String genre : genreSet) {
-            PriorityQueue<Music> pq = mmap.get(genre);
-            int cnt = 2;
-            while(!pq.isEmpty()) {
-                Music music = pq.poll();
-                result.add(music.index);
-                cnt--;
-                if(cnt == 0) {
-                    break;
-                }
+        
+        // 장르별 재생횟수가 가장 많은 곳부터 탐색 
+        for(int i=keyList.size()-1; i>=0; i--) {
+            String genre = keyList.get(i);
+            // System.out.println(genre);
+            // 가장 많이 재생된 노래 두개
+            PriorityQueue<Music> pq = gMusicMap.get(genre);
+            // 재생된 노래가 하나뿐이라면 하나의 곡만 선택 
+            if(pq.size() == 1) {
+                result.add(pq.poll().idx);
+            } else {
+                result.add(pq.poll().idx);
+                result.add(pq.poll().idx);
             }
         }
         
