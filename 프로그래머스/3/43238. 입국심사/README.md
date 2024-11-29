87b36fcdf779eac5aeaf1d94e3d2ae7d3dbd708f
Namgon <zxcv9455@naver.com>
2024-11-29 18:00:30 +0900

    [level 3] Title: 입국심사, Time: 109.48 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?itm_content=course14743) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
 
 ### 성능 요약
 
-메모리: 98.4 MB, 시간: 178.49 ms
+메모리: 110 MB, 시간: 109.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 26일 00:47:02
+2024년 11월 29일 18:00:27
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,28 +1,36 @@
 import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
-        Arrays.sort(times);
         long answer = 0;
-        long start = 0;
-        long end = times[times.length-1]*(long)n; // n명 모두 제일 오래걸리는 심사관에게 갔을 떄 
-        while(start <= end) {
-            long mid = (start+end)/2;
-            // mid 시간 내에 심사할 수 있는 최대한의 사람
-            long simsa = 0;
-            for(int time : times) {
-                // 30 -> 4 + 3 = 7명
-                simsa = simsa + (mid/time);
+        Arrays.sort(times);
+        long right = times[times.length-1] * (long)n;
+        long left = 0;
+        while(left <= right) {
+            long mid = (left + right) / 2;
+            // System.out.println(mid);
+            
+            long count = 0;
+            // mid 시점 기준으로 심사할 수 있는 사람 수가 
+            for(int i=0;i<times.length;i++) {
+                count = count + (mid / times[i]);
             }
-            // 심사한 사람이 n명보다 작으면 mid 시간안에 n명을 다 심사할 수 없음 
-            if(simsa < n) {
-                start = mid + 1;
-            }
-            // 심사한 사람이 n명 이상이면 mid 시간안에 n명 다 심사할 수 있음 
-            else {
+            // 아직 충분히 들어갈 수 있는 mid시점이라면 
+            if(count >= n) {
+                right = mid - 1;
                 answer = mid;
-                end = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
+        
         return answer;
     }
 }
+
+// t=10, [10,20,30,40,50,60]
+// t=7,  [7,14,21,28,35,42]
+// t=7,10 [7,10,14,20,21,28]
+
+// 최대로 걸리는 시간이 6명이 모두 10번대로 가는것
+// 그러면 시간을 기점으로 0,1,...60까지 했을 떄 얼마나 많은 사람이 들어올 수 있는지를 계산하면 된다. 
+// 이걸 이분 탐색으로 찾아본다. 
