8abe2a82d4fef968c063725b537e86e948aeb6e8
Namgon <zxcv9455@naver.com>
2024-11-29 20:16:31 +0900

    [level 2] Title: 타겟 넘버, Time: 4.22 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,25 +1,24 @@
 class Solution {
-    public static int cnt = 0;
-    public void dfs(int i, int n, int value, int target, int[] numbers) {
-        // n번만큼 수행했으면 종료 
-        if(i == n-1) {
-            // 최종 값이 target과 동일하면 정답 변경 
-            if(value == target) {
-                // System.out.println("i: " + i + ", value: " + value);
-                cnt+=1;
-            }
-            return;
-        }
-        
-        dfs(i+1, n, value+numbers[i+1], target, numbers);
-        
-        dfs(i+1, n, value-numbers[i+1], target, numbers);
+    public int answer = 0;
+    // 배열에 있는 값들을 모두 더하거나 빼서 target을 만들어야 한다.
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, 0, 0, target);
+        return answer;
     }
     
-    public int solution(int[] numbers, int target) {
-        int n = numbers.length;
-        dfs(-1, n, 0, target, numbers);
+    public void dfs(int[] numbers, int idx, int value, int target) {
+        // dfs 종료 조건 
+        if(idx == numbers.length) {
+            // 만약 target과 같은 값이라면 answer ++
+            if(value == target) {
+                answer++;
+            }
+            return;
+        } 
         
-        return cnt;
+        // 더하기
+        dfs(numbers, idx+1, value+numbers[idx], target);
+        // 빼기
+        dfs(numbers, idx+1, value-numbers[idx], target);
     }
 }
