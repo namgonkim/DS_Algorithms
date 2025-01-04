c689d04efe7527ee02975729dc26de59f407090f
Namgon <zxcv9455@naver.com>
2025-01-05 01:00:13 +0900

    Time: 0 ms (100%), Space: 42.2 MB (48.84%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,8 +1,15 @@
 class Solution {
-    // matrix 이외에 추가 배열을 선언하지 말고 90도 시계방향으로 회전시키기 
+    /* 7 8 9
+    *. 4 5 6
+       1 2 3
+       위,아래 뒤집고 -> i,j 를 j,i로 바꾸면 90도 회전 
+       7 4 1
+       8 5 2 
+       9 6 3
+    */
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        // 위 아래를 변경 
+        // 1. 위, 아래 뒤집기 i -> n-1-i (단, 아래를 반만 처리)
         for(int i=0;i<n/2;i++) {
             for(int j=0;j<n;j++) {
                 int temp = matrix[i][j];
@@ -10,9 +17,9 @@ class Solution {
                 matrix[n-1-i][j] = temp;
             }
         }
-        // 대각선으로 변경 
+        // 2. i,j 를 j,i로 바꾸기 (단, j를 i 값 이상(\)부터만 처리)
         for(int i=0;i<n;i++) {
-            for(int j=i+1;j<n;j++) {
+            for(int j=i;j<n;j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
